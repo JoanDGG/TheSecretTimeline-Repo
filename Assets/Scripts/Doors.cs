d8a301a3d5c26571041dc4956167822f0753c173
JoanDGG <ggjoan2000@gmail.com>
2021-07-08 17:12:04 -0500

    Escena puertas

---
+++ Assets/Scripts/Doors.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Doors : MonoBehaviour
+{
+    public void OnTriggerEnter2D(Collider2D other)
+    {
+        if (gameObject.name.Contains("Prehistoric"))
+        {
+            SceneManager.LoadScene("Prehistoria_1");
+        }
+
+        else if (gameObject.name.Contains("80"))
+        {
+            SceneManager.LoadScene("Prehistoria_1");
+        }
+
+        else if (gameObject.name.Contains("WWII"))
+        {
+            SceneManager.LoadScene("Prehistoria_1");
+        }
+
+        else if (gameObject.name.Contains("Covid"))
+        {
+            SceneManager.LoadScene("Prehistoria_1");
+        }
+
+        else if (gameObject.name.Contains("Future"))
+        {
+            SceneManager.LoadScene("Prehistoria_1");
+        }
+    }
+}
