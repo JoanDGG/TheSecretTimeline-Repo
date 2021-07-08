8ba8790fbf24a7434f1658bc006f238511e7fe36
A01378650 <65170426+A01378650@users.noreply.github.com>
2021-07-08 13:11:14 -0500

    Adelantar y Atrasar Tiempo de Objetos

---
+++ Assets/Scripts/MoveTime.cs
@@ -0,0 +1,77 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MoveTime : MonoBehaviour
+{
+
+    private bool Near;
+    private int Time;
+    public GameObject[] TimeObjects = new GameObject[3];
+    private Text Instructions;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for(int i = 0;i <= 3; i++)
+        {
+            if (this.gameObject.name.Contains(TimeObjects[i].name))
+            {
+                Time = i;
+                break;
+            }
+        }
+        Instructions = GameObject.Find("Instructions").GetComponent<Text>();
+        Near = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Near)
+        {
+            if (Input.GetKeyDown("z") && Time >= 1) // 1 or 2 / Present or Future
+            {
+                //print("Going back in time...");
+                Time--;
+                Instantiate(TimeObjects[Time], new Vector2(this.transform.position.x, this.transform.position.y), Quaternion.identity);
+                Destroy(this.gameObject);
+            }
+            else if (Input.GetKeyDown("c") && Time <= 1) // 0 or 1 / Past or Present
+            {
+                //print("Going forward in time...");
+                Time++;
+                Instantiate(TimeObjects[Time], new Vector2(this.transform.position.x, this.transform.position.y), Quaternion.identity);
+                Destroy(this.gameObject);
+            }
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Near = true;
+            Instructions.text = "Press z to rewind time\nPress c to fastforward time";
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Near = false;
+            Instructions.text = "";
+        }
+    }
+
+    private void OnTriggerStay2D(Collider2D other)
+    {
+        if (!Near && other.CompareTag("Player"))
+        {
+            Near = true;
+            Instructions.text = "Press z to rewind time\nPress c to fastforward time";
+        }
+    }
+}
